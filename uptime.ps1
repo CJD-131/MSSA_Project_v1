0fdf92f835b5ee38ea9b869d82d773fc835bb630
Ian Simons <iansimons93@gmail.com>
2024-11-18 15:16:44 -0500

    created uptime monitor

---
+++ uptime.ps1
@@ -0,0 +1,5 @@
+<#will take as input, services from a user, then check connectivity over a given time interval until stopped#>
+
+$services = read-host("please enter the services you would like to monitor separated by a comma ")
+
+
