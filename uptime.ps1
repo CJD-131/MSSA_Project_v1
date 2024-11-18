db64e1d6e2bc389e2b5d7b3cc3a4cc2a31320ac8
Ian Simons <iansimons93@gmail.com>
2024-11-18 15:23:22 -0500

    taking input and pinging input

--- uptime.ps1
+++ uptime.ps1
@@ -1,5 +1,11 @@
 <#will take as input, services from a user, then check connectivity over a given time interval until stopped#>
 
-$services = read-host("please enter the services you would like to monitor separated by a comma ")
+$raw_services = read-host("please enter the IPv4 addresses of services you would like to monitor, separated by a comma ")
+
+$services = $raw_services -split (",")
+
+for ($i = 0; $i -lt $services.Length; $i++){
+	ping $services[$i]
+}
 
 
